1443ac3de5b8d226415085b72a7ce4bfa0a330d4
宋文通 <275712575@qq.com>
2020-05-01 16:36:56 +0800

    init Project

---
+++ WeChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeChat
+//
+//  Created by Wentong Song on 2020/5/1.
+//  Copyright © 2020 Wentong Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
